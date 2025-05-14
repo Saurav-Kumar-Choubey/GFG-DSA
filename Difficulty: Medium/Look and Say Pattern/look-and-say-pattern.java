6af44bc8c88d08240d165fbad7b773f774ba085c
Saurav Kumar Choubey <122750662+Saurav-Kumar-Choubey@users.noreply.github.com>
2025-05-14 23:29:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Look and Say Pattern/look-and-say-pattern.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+
+            System.out.println(ob.countAndSay(n));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public String countAndSay(int n) {
+        String result = "1";
+        for (int i = 1; i < n; i++) {
+            StringBuilder next = new StringBuilder();
+            int count = 1;
+            for (int j = 1; j < result.length(); j++) {
+                if (result.charAt(j) == result.charAt(j - 1)) {
+                    count++;
+                } else {
+                    next.append(count).append(result.charAt(j - 1));
+                    count = 1;
+                }
+            }
+            next.append(count).append(result.charAt(result.length() - 1));
+            result = next.toString();
+        }
+        return result;
+    }
+}
+
