ff5141beae643ad204398608240047e137f87829
Saurav Kumar Choubey <122750662+Saurav-Kumar-Choubey@users.noreply.github.com>
2025-06-11 23:58:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove the balls/remove-the-balls.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findLength(int[] color, int[] radius) {
+        // code here
+        
+         Stack<Integer> colorStack = new Stack<>();
+        Stack<Integer> radiusStack = new Stack<>();
+        
+        for (int i = 0; i < color.length; i++) {
+            if (!colorStack.isEmpty() && colorStack.peek() == color[i] && radiusStack.peek() == radius[i]) {
+                colorStack.pop();
+                radiusStack.pop();
+            } else {
+                colorStack.push(color[i]);
+                radiusStack.push(radius[i]);
+            }
+        }
+        
+        return colorStack.size();
+        
+    }
+}
